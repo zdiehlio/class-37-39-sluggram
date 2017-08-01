78c625a227badd1ca9088ae105fb4dba91f916ce
Zachary Diehl <zachary.diehl@gmail.com>
2017-08-01 15:41:17 -0700

    created signup and login functionality using backend authorization

---
+++ frontend/src/action/auth-actions.js
@@ -0,0 +1,34 @@
+import superagent from 'superagent'
+
+export const tokenSet = (token) => ({
+  type: 'TOKEN_SET',
+  payload: token,
+})
+
+export const tokenDelete = () => ({ type: 'TOKEN_DELETE' })
+
+export const signupRequest =  (user) => (dispatch) => {
+  return superagent.post(`${__API_URL__}/signup`)
+    .withCredentials()
+    .send(user)
+    .then(res => {
+      dispatch(tokenSet(res.text))
+      //res.text === toke
+      try {
+        localStorage.token = res.text
+      } catch (error) {
+        console.log(error)
+      }
+      return res
+    })
+}
+
+export const loginRequest = (user) => (dispatch) => {
+  return superagent.get(`${__API_URL__}/login`)
+    .withCredentials()
+    .auth(user.username, user.password)
+    .then(res => {
+      dispatch(tokenSet(res.text))
+      return res
+    })
+}
